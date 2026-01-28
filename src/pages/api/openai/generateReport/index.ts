4a64a1a67c0e37342e264357cca92ff9e5079609
Sammy El-Sherif <elsherifsammy@gmail.com>
2026-01-28 14:28:27 -0800

    consolidate

--- src/pages/api/openai/generateReport/index.ts
+++
@@ -1,7 +0,0 @@
-import { generateReportRoute } from '@/api/routes/openai'
-import { HttpMethod } from '@/interfaces/httpMethod'
-import { apiRequestWrapper } from '@/server/apiRequestWrapper'
-
-export default apiRequestWrapper({
-  [HttpMethod.POST]: generateReportRoute,
-})
