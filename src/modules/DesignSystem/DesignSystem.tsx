ba7efd33b712125dcdfc07ad33aad1e04bca0e5e
Sammy El-Sherif <elsherifsammy@gmail.com>
2026-01-28 20:40:23 -0800

    adding logging

--- src/modules/DesignSystem/DesignSystem.tsx
+++ src/modules/DesignSystem/DesignSystem.tsx
@@ -27,7 +27,12 @@ export const DesignSystem = () => {
     setTimeout(() => setIsLoading(false), 2000)
   }
   const currentVersion = process.env.WDS_VERSION
-
+  console.log('SIGNIN PAGE', process.env.NEXTAUTH_URL)
+  console.log('SIGNIN PAGE', process.env.HOST)
+  console.log('SIGNIN PAGE', process.env.NODE_ENV)
+  console.log('SIGNIN PAGE', process.env.AUTH0_ISSUER_BASE_URL)
+  console.log('SIGNIN PAGE', process.env.AUTH0_SCOPE)
+  console.log('SIGNIN PAGE', process.env.AUTH0_AUDIENCE)
   return (
     <div className={styles.page}>
       {/* Header */}
