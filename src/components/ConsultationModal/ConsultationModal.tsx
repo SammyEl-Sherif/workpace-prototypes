4f5b9ba1b2c80abfe1bf1be6e88cc52269254a70
Sammy El-Sherif <elsherifsammy@gmail.com>
2026-04-04 18:52:12 -0700

    update release design system workflow to not have redundant branch filed

--- src/components/ConsultationModal/ConsultationModal.tsx
+++ src/components/ConsultationModal/ConsultationModal.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+t import { useCallback, useEffect, useRef, useState } from 'react'
 
 import { Button, InputField, Select, Text } from '@workpace/design-system'
 import cn from 'classnames'
